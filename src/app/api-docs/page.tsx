102d82dc96b8608d52a8051702a211874dd6d43d
junifar <junifar@gmail.com>
2024-05-10 03:25:59 +0700

    chore

--- src/app/api-docs/page.tsx
+++ src/app/api-docs/page.tsx
@@ -4,7 +4,7 @@ export default function ApiDocsPage() {
     return (
         <div className="w-full">
             <div className="h-full lg:ml-72 xl:ml-80">
-                <Header />
+                {Header()}
                 {ContentPage()}
             </div>
         </div>
@@ -586,7 +586,7 @@ function GreenBackground() {
     </div>;
 }
 
-export function Header() {
+function Header() {
     return (
         <header className="contents lg:pointer-events-none lg:fixed lg:inset-0 lg:z-40 lg:flex">
             <div className="contents lg:pointer-events-auto lg:block lg:w-72 lg:overflow-y-auto lg:border-r lg:border-zinc-900/10 lg:px-6 lg:pb-8 lg:pt-4 xl:w-80 lg:dark:border-white/10">
